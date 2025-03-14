38bc57ec74a51352a92d4487e14b2424949f6b76
Walmir Sousa <walmir.sousaa@gmail.com>
2025-03-14 11:38:51 -0300

    LOGIN MAX

--- index.php
+++ index.php
@@ -11,28 +11,39 @@
         body {
             display: flex;
             justify-content: center;
+            align-items: center;
+            min-height: 100vh;
             background-color: #0e0e0e;
             color: #f2f2f2;
         }
  
-        form {
+        .card {
+            background-color: #1f1f1f;
+            color: #f2f2f2;
+            border-radius: 15px;
+            padding: 20px;
             width: 500px;
+            box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.5);
         }
     </style>
 
 <body>
  
-    <form action="auxlogin.php" method="POST" class="mt-5">
+    <div class="card">
+        <form action="auxlogin.php" method="POST" class="mt-3">
 
-        <h1>login</h1>
-        <label class="form-label"> Usuário: </label>
-        <input type="text" class="form-control" name="user">
-        
-        <label class="form-label">senha:</label>
-        <input type="text" class="form-control" name="password">
+            <h1 class="text-center">Login</h1>
+            <label class="form-label">Usuário:</label>
+            <input type="text" class="form-control" name="user">
+            
+            <label class="form-label mt-3">Senha:</label>
+            <input type="password" class="form-control" name="password">
 
-        <input class="btn btn-success mt-3" type="submit">
-    </form>
+            <div class="d-flex justify-content-between mt-4">
+                <input class="btn btn-success" type="submit" value="Entrar">
+                <a href="cadastro.php" class="btn btn-primary">Cadastrar</a>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
- 
